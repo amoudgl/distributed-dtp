3dd8cdb63e1f0197e79bbbcc7e8ed93a1ade3973
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-04-25 18:15:16 -0400

    rename main method to run

--- main.py
+++ main.py
@@ -8,35 +8,37 @@ from torch import nn
 
 
 @hydra.main(config_path="conf", config_name="config")
-def main(cfg: DictConfig) -> None:
+def run(cfg: DictConfig):
     # print run info
     print(OmegaConf.to_yaml(cfg))
     print("Working directory: {}".format(os.getcwd()))
 
     # create datamodule
-    datamodule = instantiate(cfg.datamodule,
-                             data_dir=cfg.data_dir)
+    datamodule = instantiate(cfg.datamodule, data_dir=cfg.data_dir)
 
     # create network
-    network: nn.Sequential = instantiate(cfg.network,
-                                         in_channels=datamodule.dims[0],
-                                         n_classes=datamodule.num_classes)
+    network: nn.Sequential = instantiate(
+        cfg.network, in_channels=datamodule.dims[0], n_classes=datamodule.num_classes
+    )
 
     # create model
     # note: _recursive_ is set to False below to avoid creating optimizers
     # since they are created internally in configure_optimizers() method
     # of lightning module.
-    model = instantiate(cfg.model,
-                        _recursive_=False,
-                        datamodule=datamodule,
-                        network=network,
-                        hparams=cfg.model,
-                        full_config=cfg,
-                        network_hparams=cfg.network)
+    model = instantiate(
+        cfg.model,
+        _recursive_=False,
+        datamodule=datamodule,
+        network=network,
+        hparams=cfg.model,
+        full_config=cfg,
+        network_hparams=cfg.network,
+    )
 
     # create trainer
     if cfg.debug:
         # define trainer debug behavior
+        cfg.trainer.max_epochs = 1
         cfg.trainer.accelerator = None
         cfg.trainer.gpus = 1
         cfg.trainer.logger = None  # disable wandb logging
@@ -58,6 +60,6 @@ def main(cfg: DictConfig) -> None:
     print(f"Test top5 accuracy: {top5_accuracy:.1%}")
     return top1_accuracy, top5_accuracy
 
+
 if __name__ == "__main__":
-    main()
-    
+    run()
