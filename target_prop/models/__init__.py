40e45f0605d50770438b04cba50be381f620787c
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-05-05 14:57:27 -0400

    [wip] layer parallel DTP

--- target_prop/models/__init__.py
+++ target_prop/models/__init__.py
@@ -1,6 +1,7 @@
 # from .model import BaseModel
 from .baseline import BaselineModel
 from .dtp import DTP
+from .layer_parallel_dtp import LayerParallelDTP
 from .parallel_dtp import ParallelDTP
 from .tp import TargetProp
 from .vanilla_baseline import VanillaBaselineModel
--- target_prop/trainers/layer_parallel.py
+++ target_prop/trainers/layer_parallel.py
@@ -2,11 +2,18 @@ import torch
 
 
 class LayerParallelTrainer:
+    """
+    Multi-GPU layer parallel trainer for DTP.
+
+    Works with DTP layer parallel model, can be tested with the following command:
+    python main.py model=layer_parallel_dtp trainer=layer_parallel scheduler=cosine network=simple_vgg datamodule=cifar10
+    """
+
     def __init__(self, gpus, max_epochs, logger) -> None:
-        # self.max_epochs = max_epochs
-        # self.gpus = gpus
-        # self.logger = logger
-        # setup DDP processes
+        self.max_epochs = max_epochs
+        self.gpus = gpus
+        self.logger = logger
+        self.device = "cpu" if not torch.cuda.is_available() else torch.cuda.current_device()
         pass
 
     def fit(self, model, datamodule, rank=0):
@@ -20,7 +27,7 @@ class LayerParallelTrainer:
         #     # do feedback training according to rank
         #     for batch in
 
-        #     # all_gather params or just gather
+        #     # all_gather feedback params or just gather
 
         #     # forward + backward step
 
