fcbf72237ed68c68a519f57e6ed6168a504fb95a
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-05-05 18:13:21 -0400

    [wip] add dist init

--- target_prop/trainers/layer_parallel.py
+++ target_prop/trainers/layer_parallel.py
@@ -1,4 +1,16 @@
+import os
+
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+
+def init_process(rank, size, fn, backend, *args):
+    """Initialize the distributed environment."""
+    os.environ["MASTER_ADDR"] = "127.0.0.1"
+    os.environ["MASTER_PORT"] = "9000"
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size, *args)
 
 
 class LayerParallelTrainer:
@@ -14,11 +26,21 @@ class LayerParallelTrainer:
         self.gpus = gpus
         self.logger = logger
         self.device = "cpu" if not torch.cuda.is_available() else torch.cuda.current_device()
-        pass
 
-    def fit(self, model, datamodule, rank=0):
-        pass
-        # # sync params across processes
+    def fit(self, model, datamodule):
+        # setup distributed processes
+        processes = []
+        mp.set_start_method("spawn")
+        size = self.gpus
+        for rank in range(size):
+            p = mp.Process(
+                target=init_process, args=(rank, size, self.fit_worker, "nccl", model, datamodule)
+            )
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
+        # broadcast params from rank 0 to all other processes
         # model.train()
 
         # # training loop
@@ -32,3 +54,12 @@ class LayerParallelTrainer:
         #     # forward + backward step
 
         #     # (optional) broadcast params to other devices
+
+    def fit_worker(self, rank, size, model, datamodule):
+        print(
+            rank,
+        )
+
+    def test(self, model, datamodule, verbose=False):
+        # verbose argument is just a dummy argument to match lightning format
+        pass
