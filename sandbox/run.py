1195dd921e6970c55b8ab65edf6a530f7359a469
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-05-11 10:20:45 -0400

    fixes for nccl backend

--- sandbox/run.py
+++ sandbox/run.py
@@ -9,18 +9,21 @@ import torch.multiprocessing as mp
 
 def run(rank, size):
     """Distributed function to be implemented later."""
+    device = torch.device("cuda:{}".format(rank % size))
+    torch.cuda.set_device(device)
     if rank == 0:
         tensor = torch.tensor([1, 2])
         # Send the tensor to process 1
     else:
         # Receive tensor from process 0
         tensor = torch.tensor([3])
+    tensor = tensor.to(device)
     tensor_list = [None, None]
     dist.all_gather_object(tensor_list, tensor)
     print(tensor_list)
 
 
-def init_process(rank, size, fn, backend="gloo"):
+def init_process(rank, size, fn, backend="nccl"):
     """Initialize the distributed environment."""
     os.environ["MASTER_ADDR"] = "127.0.0.1"
     os.environ["MASTER_PORT"] = "29500"
@@ -30,10 +33,12 @@ def init_process(rank, size, fn, backend="gloo"):
 
 if __name__ == "__main__":
     size = 2
+    backend = "nccl"
     processes = []
+    print(f"using backend: {backend}")
     mp.set_start_method("spawn")
     for rank in range(size):
-        p = mp.Process(target=init_process, args=(rank, size, run))
+        p = mp.Process(target=init_process, args=(rank, size, run, backend))
         p.start()
         processes.append(p)
 
